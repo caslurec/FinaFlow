e34c6303652235549de0539ba93d99b952a4e911
Lucas Sousa <lucassousa4421@gmail.com>
2024-06-04 23:25:22 -0300

    subindo meu primeiro projeto

---
+++ Fina.Core/Models/Transaction.cs
@@ -0,0 +1,20 @@
+using Fina.Core.Enums;
+
+namespace Fina.Core.Models;
+
+public class Transaction
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = String.Empty;
+
+    public DateTime CreatedAt { get; set; } = DateTime.Now;           
+    public DateTime? PaidOrReceivedAt { get; set; }
+    
+    public EtransactionType Type { get; set; } = EtransactionType.Withdraw;
+    
+    public long CategoryId { get; set; }
+    public Category Category { get; set; } = null;
+
+    public string UserId { get; set; } = string.Empty;
+
+}
